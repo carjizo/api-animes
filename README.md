4ef02f72b34cdc982785c2ddbc1c07f2e3a96690
carjiso <cjimenez@indigitalsolutions.com>
2024-09-01 23:06:51 -0500

    add docs

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # api-animes
-Api animes
+API to get the links of the servers of the episodes of an anime.
+
+Library that was used:
+https://github.com/jorgeajimenezl/animeflv-api
+### Documentacion
+#### Local 
+http://localhost:8000/docs
+#### Prod
+https://api-animes-9d7a.onrender.com/docs
--- src/controllers/anime_controller.py
+++ src/controllers/anime_controller.py
@@ -13,10 +13,19 @@ class AnimeController():
 
     @anime_controller.post('/links', tags=['animes'], response_model=list[AnimeResponse], status_code=200)
     def get_links(anime: AnimeRequest):
+        """
+        Get likns server animes\n
+        parameter id: id del anime\n
+        parameter episode: número de episodios
+        """
         links = AnimeService.get_links(anime)
         return JSONResponse(status_code=200, content=jsonable_encoder(links))
     
     @anime_controller.get('/find-by-name/{name_anime}', tags=['animes'], response_model=list[AnimeDTO], status_code=200)
     def find_by_anime(name_anime: str):
+        """
+        Find by name anime\n
+        parameter name_anime: nombre random de algún anime
+        """
         animes = AnimeService.find_by_name(name_anime)
         return JSONResponse(status_code=200, content=jsonable_encoder(animes))
